dd7d00654ad99e20d966257628a8809c8dad9d6c
Suh Donghwi <hwidongsuh@gmail.com>
2020-06-30 10:25:56 +0900

    feat: add navigation bar components, set baseUrl option

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import NavBar from 'components/NavBar';
+
 function App() {
   return (
     <div className="App">
-      안녕, 디미고!
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Nav = styled.nav`
+`;
+
+function NavBar() {
+  return <Nav>1376 홍길동</Nav>
+}
+
+export default NavBar;
