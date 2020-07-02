dbc59034f04b23702242075d959af15c011fe833
Suh Donghwi <hwidongsuh@gmail.com>
2020-07-02 15:59:19 +0900

    feat: add logo image to navigation bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,10 +2,23 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Nav = styled.nav`
+  border-bottom: 1px solid black;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+const LogoImage = styled.img`
+  width: 3rem;
 `;
 
 function NavBar() {
-  return <Nav>1376 홍길동</Nav>
+  return (
+    <Nav>
+      <LogoImage src="/council-logo.png" />
+
+    </Nav>
+  );
 }
 
 export default NavBar;
