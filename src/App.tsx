0d75cf6763ff5fd77fba70563979a0813f6d645c
Suh Donghwi <hwidongsuh@gmail.com>
2020-07-05 05:37:48 +0900

    feat: add petition status component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import NavBar from 'components/NavBar';
 import Banner from 'components/Banner';
+import PetitionStatus from 'components/PetitionStatus';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Banner />
+      <PetitionStatus />
     </div>
   );
 }
---
+++ src/components/PetitionStatus.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import colors from 'assets/colors';
+import devices from 'assets/devices';
+
+import { FiAlertCircle, FiClock, FiCheckCircle } from 'react-icons/fi';
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+
+  margin: 1rem 0;
+  @media ${devices.tablet} {
+    flex-direction: column;
+  }
+`;
+
+const Status = styled.div`
+  display: grid;
+  grid-template-columns: 70px 1fr;
+  grid-template-rows: 30px 30px;
+  align-items: center;
+
+  margin: 1rem 2.4rem;
+`;
+
+const Icon = styled.div`
+  font-size: 3.5rem;
+  color: ${colors.main};
+
+  grid-row: 1/3;
+  margin-top: 10px;
+`;
+
+const Title = styled.div`
+  font-size: 1.3rem;
+`;
+
+const Number = styled.p`
+  font-size: 1.3rem;
+  color: ${colors.main};
+`;
+
+function PetitionStatus() {
+  return (
+    <Container>
+      <Status>
+        <Icon>
+          <FiAlertCircle />
+        </Icon>
+        <Title>등록된 청원</Title>
+        <Number>10,500</Number>
+      </Status>
+
+      <Status>
+        <Icon>
+          <FiClock />
+        </Icon>
+        <Title>진행 중인 청원</Title>
+        <Number>10,500</Number>
+      </Status>
+
+      <Status>
+        <Icon>
+          <FiCheckCircle />
+        </Icon>
+        <Title>답변된 청원</Title>
+        <Number>10,500</Number>
+      </Status>
+    </Container>
+  );
+}
+
+export default PetitionStatus;
