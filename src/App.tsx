104b32f2cd876d5883440f7bef9b94b2dd755663
Suh Donghwi <hwidongsuh@gmail.com>
2020-07-05 05:00:28 +0900

    feat: add banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import NavBar from 'components/NavBar';
+import Banner from 'components/Banner';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Banner />
     </div>
   );
 }
---
+++ src/components/Banner.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import devices from 'assets/devices';
+
+const Container = styled.div`
+  width: 100%;
+  height: 450px;
+
+  background-image: url('/school.png');
+  background-position: center;
+  box-shadow: inset 0 0 0 2000px rgba(255, 0, 150, 0.3);
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+`;
+
+const TextContainer = styled.div`
+  color: white;
+
+  width: 50%;
+
+  @media ${devices.laptopL} {
+    width: 80%;
+  }
+
+  @media ${devices.laptop} {
+    width: 90%;
+  }
+`;
+
+const Description = styled.p`
+  margin: 0;
+  font-size: 1.2rem;
+`;
+
+const MainHeading = styled.h1`
+  font-size: 3rem;
+  font-weight: normal;
+  margin: 0.5rem 0 0.7rem 0;
+
+  word-break: keep-all;
+`;
+
+function Banner() {
+  return (
+    <Container>
+      <TextContainer>
+        <Description>학생청원솔루션</Description>
+        <MainHeading>
+          디미고 온라인 건의함 서비스<br />
+          <strong>디미청원</strong>
+        </MainHeading>
+        <Description>지금 바로 학교를 변화시켜보세요.</Description>
+      </TextContainer>
+    </Container>
+  );
+}
+
+export default Banner;
