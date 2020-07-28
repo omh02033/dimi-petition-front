7cc867b12ab4f48f66198d92d11b8f521265032e
Suh Donghwi <hwidongsuh@gmail.com>
2020-07-29 07:24:29 +0900

    chore: move nav, banner, container to router index

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,42 +1,18 @@
 import React from 'react';
-import styled from 'styled-components';
 
-import devices from 'assets/devices';
-
-import NavBar from 'components/NavBar';
-import Banner from 'components/Banner';
 import PetitionStatus from 'components/PetitionStatus';
 import PetitionSteps from 'components/PetitionSteps';
 import PetitionList from 'components/PetitionList';
-import Footer from 'components/Footer';
-
-const Container = styled.main`
-  margin: 0 auto;
-  width: 50%;
-
-  @media ${devices.laptopL} {
-    width: 80%;
-  }
-
-  @media ${devices.laptop} {
-    width: 90%;
-  }
-`;
 
 function Home() {
   return (
-    <main>
-      <NavBar />
-      <Banner />
-      <Container>
-        <PetitionStatus />
-        <PetitionSteps />
-        <PetitionList title="진행중인 청원" perPage={5} />
-        <PetitionList title="답변 대기 중인 청원" perPage={5} />
-        <PetitionList title="만료된 청원" perPage={5} />
-      </Container>
-      <Footer />
-    </main>
+    <>
+      <PetitionStatus />
+      <PetitionSteps />
+      <PetitionList title="진행중인 청원" perPage={5} />
+      <PetitionList title="답변 대기 중인 청원" perPage={5} />
+      <PetitionList title="만료된 청원" perPage={5} />
+    </>
   );
 }
 
--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,13 +1,43 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import styled from 'styled-components';
+
+import devices from 'assets/devices';
+
+import NavBar from 'components/NavBar';
+import Banner from 'components/Banner';
+import Footer from 'components/Footer';
+
 import Home from 'pages/Home';
+
+const Container = styled.main`
+  margin: 0 auto;
+  width: 50%;
+
+  @media ${devices.laptopL} {
+    width: 80%;
+  }
+
+  @media ${devices.laptop} {
+    width: 90%;
+  }
+`;
 
 const Root = () => (
   <BrowserRouter>
-    <Switch>
-      <Route path="/" exact component={Home} />
-    </Switch>
+    <NavBar />
+    <Banner />
+
+    <main>
+      <Container>
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </Container>
+    </main>
+
+    <Footer />
   </BrowserRouter>
 )
 
