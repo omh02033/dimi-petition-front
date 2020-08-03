1354554f106f3c8fcb9a8e513ec99309c511869d
Suh Donghwi <hwidongsuh@gmail.com>
2020-08-03 21:04:47 +0900

    feat: make filter working in rest pages

--- src/pages/AnsweredPage.tsx
+++ src/pages/AnsweredPage.tsx
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import CategorySelect from 'components/CategorySelect';
 import PetitionStatus from 'components/PetitionStatus';
 import PetitionList from 'components/PetitionList';
 import ComponentTitle from 'components/ComponentTitle';
 
-const PopularityPage = () => (
-  <>
+import Category from 'data/Category';
+
+const PopularityPage = () => {
+  const [filter, setFilter] = useState<Category | null>(null);
+
+  return (<>
     <PetitionStatus />
 
     <ComponentTitle>분야별 청원 답변 보기</ComponentTitle>
-    <CategorySelect />
+    <CategorySelect onChangeSelect={(c) => setFilter(c)} />
 
-    <PetitionList title="전체" perPage={10} />
-  </>
-);
+    <PetitionList title="전체" perPage={10} categoryFilter={filter} />
+  </>);
+};
 
 export default PopularityPage;
--- src/pages/PopularityPage.tsx
+++ src/pages/PopularityPage.tsx
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import CategorySelect from 'components/CategorySelect';
 import PetitionStatus from 'components/PetitionStatus';
 import PetitionList from 'components/PetitionList';
 import ComponentTitle from 'components/ComponentTitle';
 
-const PopularityPage = () => (
-  <>
+import Category from 'data/Category';
+
+const PopularityPage = () => {
+  const [filter, setFilter] = useState<Category | null>(null);
+
+  return (<>
     <PetitionStatus />
 
     <ComponentTitle>청원 추천순 보기</ComponentTitle>
-    <CategorySelect />
+    <CategorySelect onChangeSelect={(c) => setFilter(c)} />
 
-    <PetitionList title="진행중인 청원" perPage={8} />
-    <PetitionList title="답변 대기 중인 청원" perPage={5} />
-  </>
-);
+    <PetitionList title="진행중인 청원" perPage={8} categoryFilter={filter} />
+    <PetitionList title="답변 대기 중인 청원" perPage={5} categoryFilter={filter} />
+  </>);
+};
 
 export default PopularityPage;
