d1883d06ec63b44eea7fa514b9883791ec50b496
Suh Donghwi <hwidongsuh@gmail.com>
2020-08-07 08:38:50 +0900

    feat: add login banner content

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -16,7 +16,6 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-
 `;
 
 const TextContainer = styled.div`
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -13,10 +13,34 @@ const Banner = styled.section`
   background-image: url('/school-full.png');
   background-position: center;
   background-size: cover;
-  box-shadow: inset 0 0 0 2000px rgba(255, 0, 150, 0.4);
+  box-shadow: inset 0 0 0 2000px rgba(255, 0, 150, 0.6);
 
   height: 100%;
   width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const BannerTitle = styled.h1`
+  color: white;
+  font-size: 3rem;
+
+  margin: 1rem 0 0.5rem 0;
+`;
+
+const BannerSubtitle = styled.h2`
+  color: white;
+  font-size: 1.5rem;
+  font-weight: normal;
+
+  margin: 0;
+`;
+
+const BannerDescription = styled.p`
+  color: white;
 `;
 
 const LoginSection = styled.section`
@@ -25,8 +49,15 @@ const LoginSection = styled.section`
 
 const LoginPage = () => (
   <Container>
-    <Banner></Banner>
-    <LoginSection></LoginSection>
+    <Banner>
+      <img src="/council-logo-white.svg" alt="학생회 로고" />
+      <BannerTitle>디미청원</BannerTitle>
+      <BannerSubtitle>디미고 온라인 건의함 서비스</BannerSubtitle>
+      <BannerDescription>지금 바로 학교를 변화시켜보세요.</BannerDescription>
+    </Banner>
+
+    <LoginSection>
+    </LoginSection>
   </Container>
 );
 
