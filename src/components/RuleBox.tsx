9422e962e6677ce23f12e41c17a734ca0383b628
Suh Donghwi <hwidongsuh@gmail.com>
2020-08-10 14:39:18 +0900

    feat: add every rule box

---
+++ src/components/RuleBox.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import colors from 'assets/colors';
+
+interface RuleBoxProps {
+  title: string;
+  content: string;
+  groupName: string;
+}
+
+const Container = styled.section`
+  margin: 0.5rem 0;
+`;
+
+const Title = styled.h2`
+  font-size: 1.3rem;
+  color: ${colors.textMain};
+`;
+
+const Content = styled.p`
+  border: 2px solid #D9D9D9;
+  padding: 1rem;
+  color: ${colors.textMain};
+`;
+
+const InputContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const AgreeText = styled.p`
+  margin: 0 1rem 3px auto;
+`;
+
+const RuleBox = ({ title, content, groupName }: RuleBoxProps) => (
+  <Container>
+    <Title>{title}</Title>
+    <Content>{content}</Content>
+
+    <InputContainer>
+      <AgreeText>동의하십니까?</AgreeText>
+      <div className="pretty p-default p-round">
+        <input type="radio" name={groupName} />
+        <div className="state p-primary-o">
+          <label>예</label>
+        </div>
+      </div>
+      <div className="pretty p-default p-round">
+        <input type="radio" name={groupName} />
+        <div className="state p-primary-o">
+          <label>아니요</label>
+        </div>
+      </div>
+    </InputContainer>
+  </Container>
+);
+
+export default RuleBox;
