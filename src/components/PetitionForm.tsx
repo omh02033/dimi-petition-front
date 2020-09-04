1d1f3c46af52bb5d716df6a200f82b277c0a7c6f
suhdonghwi <hwidongsuh@gmail.com>
2020-09-04 09:20:54 +0900

    feat: add petition content input

--- src/components/PetitionForm.tsx
+++ src/components/PetitionForm.tsx
@@ -12,7 +12,7 @@ const Label = styled.label`
   font-weight: bold;
   font-size: 1.3rem;
 
-  margin-bottom: 0.5rem;
+  margin-bottom: 1rem;
 `;
 
 const Control = styled.div`
@@ -22,10 +22,17 @@ const Control = styled.div`
   margin-bottom: 3rem;
 `;
 
-const TextInput = styled.input`
+const TitleInput = styled.input`
   border: 1px solid #D9D9D9;
   font-size: 1.3rem;
   padding: 0.8rem 0.8rem;
+`;
+
+const ContentInput = styled.textarea`
+  border: 1px solid #D9D9D9;
+  font-size: 1.1rem;
+  padding: 0.8rem 0.8rem;
+  resize: none;
 `;
 
 const PetitionForm = () => {
@@ -33,12 +40,17 @@ const PetitionForm = () => {
     <Form>
       <Control>
         <Label htmlFor="title">청원제목</Label>
-        <TextInput placeholder="청원 제목을 입력하세요." type="text" id="title" />
+        <TitleInput placeholder="청원 제목을 입력하세요." type="text" id="title" />
       </Control>
 
       <Control>
-        <Label htmlFor="title">분야(카테고리)</Label>
+        <Label>분야(카테고리)</Label>
         <CategorySelect />
+      </Control>
+
+      <Control>
+        <Label htmlFor="content">청원내용</Label>
+        <ContentInput placeholder="청원 내용을 입력하세요." id="content" rows={15}/>
       </Control>
     </Form>
   )
