1c99d31082e3eac0942afdbaf28000720f2d6562
Suh Donghwi <hwidongsuh@gmail.com>
2021-06-25 18:35:05 +0900

    feat: change list order of Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,18 +12,18 @@ const Home = () => {
       <PetitionSteps />
       <PetitionListContainer
         title="진행중인 청원"
-        perPage={8}
-        statusFilter={Status.Progress}
-      />
-      <PetitionListContainer
-        title="답변 대기 중인 청원"
         perPage={5}
-        statusFilter={Status.Waiting}
+        statusFilter={Status.Progress}
       />
       <PetitionListContainer
         title="답변된 청원"
         perPage={5}
         statusFilter={Status.Answered}
+      />
+      <PetitionListContainer
+        title="답변 대기 중인 청원"
+        perPage={5}
+        statusFilter={Status.Waiting}
       />
     </>
   );
